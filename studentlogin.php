83a7e993d8565e70438b89fc6e541260f556e46d
nikhilreddyjan <nikhil.reddyjan98@gmail.com>
2018-07-13 21:14:20 +0530

    Update studentlogin.php

--- studentlogin.php
+++ studentlogin.php
@@ -1,9 +1,9 @@
 <?php include("top1.php"); ?>
 <html>
 
-<table>
+<table style="width:100%;">
 <tr>
-<td style="padding:8px;padding-bottom:250px;">
+<td style="padding:68px;padding-bottom:50px;border-right:1px solid black;height:500px;">
 <?php include("left1.php");?>
 </td>
 <td style="width:100%;">
